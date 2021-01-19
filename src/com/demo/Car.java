9a393fdc3dfc01220b98ee9d307511860c3bdf8b
Git_Srikanth <harshactrl@gmail.com>
2021-01-19 10:54:16 +0530

    JIRA:120

---
+++ src/com/demo/Car.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Car {
+
+	static int a =10;
+	static int b =20;
+	static int c = a+b;
+	
+	public static void main(String[] args) {
+		 
+		System.out.println(c);
+	}
+	
+}
